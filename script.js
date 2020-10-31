e10e92e39f7e1918b4088a419431281ac1140551
D-T-666 <tabatadzedima20@gmail.com>
2020-10-31 21:33:20 +0400

    restart cleanup

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-let WFC;
-
 function setup() {
     createCanvas(400, 400);
-
-    WFC = CreateField('data/demo-1.png', {
-        N: 2,
-        W: 16,
-        H: 16,
-        Symmetry: false
-    });
-
-    WFC
-
 }
 
 function draw() {
-
-    for (let row of WFC.grid) {
-        for (let elt of row) {
-            elt.reveal_step(frameCount);
-            elt.show();
-        }
-    }
+    background(51);
 }
