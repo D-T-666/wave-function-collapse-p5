306669548d12153ad5c3814b3399b7db9b99002b
Dimitri Tabatadze <tabatadzedima20@gmail.com>
2020-12-02 23:55:53 +0400

    🧹💪 major cleanup

--- sketch/tiles.js
+++ sketch/tiles.js
@@ -4,22 +4,16 @@ class Tile {
     this.pLen = this.states.length;
     this.total_states = total_states;
     this.pos = createVector(x || 0, y || 0);
-    this.highlight = false;
+    this._hasCollapsed = false;
   }
 
   collapse() {
     this._hasCollapsed = true;
-    // Picks a random state and makes it the only state in the list
+    // Picks a random state and makes it the only one in the list
     this.states = [random(this.states)];
   }
 
-  checkCollapsed() {
-    if (!this._hasCollapsed) this._hasCollapsed = this.states.length == 1;
-  }
-
   hasCollapsed() {
-    // Checks if the length of the possible states is 1 
-    // which would mean that the tile has collapsed
     return this._hasCollapsed;
   }
 
@@ -27,13 +21,6 @@ class Tile {
     // Returns infinity if the tile has collapsed and returns the 
     // length of the states if the tile hasn't collapsed
     return (this.pLen > 1) ? this.pLen : Infinity;
-  }
-
-  slowReveal(dir = 0) {
-    // initiallizes some varialbes for animation
-    this.reveal_direction = dir;
-    this.reveal_timer_max = 10;
-    this.reveal_timer = this.reveal_timer_max;
   }
 
   display() {
@@ -45,59 +32,32 @@ class Tile {
     let w = tileW;
     let h = tileH;
 
-    if (this.hasCollapsed()) {
-      if (this.reveal_timer > 0.02) {
-        if (this.color != background_color || displayBackgroundTiles) {
+    if (this.pLen > 1) {
+      if (this.hasCollapsed()) {
+        // fill(background_color);
+        // stroke(background_color);
+        // drawCell(x, y, w, h, true);
+
+        stroke(this.color);
+        fill(this.color);
+        drawCell(x, y, w, h);
+
+        this.pLen = 1;
+      } else {
+        if (this.states.length !== this.pLen) {
           // fill(background_color);
           // stroke(background_color);
-          // drawCell(x, y, w, h);
+          // drawCell(x, y, w, h, true);
 
-          noStroke();
-
-          this.reveal_timer = lerp(this.reveal_timer, 0, 0.99);
-
-          stroke(this.color);
-          fill(this.color);
+          this.color.setAlpha(map(constrain(this.pLen, 0, 100), 0, 100, 210, 0));
+          fill(this.color || color(255, 0, 0));
+          stroke(this.color || color(255, 0, 0));
           drawCell(x, y, w, h);
-
         }
       }
-      this.pLen = 1;
-
-    } else {
-      if (this.pLen > 1 && this.states.length - this.pLen != 0) {
-        fill(background_color);
-        stroke(background_color);
-        drawCell(x, y, w, h);
-
-
-        this.color.setAlpha(map(constrain(this.pLen, 0, 100), 0, 100, 255, 0));
-        fill(this.color || color(255, 0, 0, map(constrain(this.pLen, 0, 50), 0, 50, 255, 0)));
-        stroke(this.color || color(255, 0, 0, map(constrain(this.pLen, 0, 50), 0, 50, 255, 0)));
-        drawCell(x, y, w, h);
-      }
+      // Update pLen.
+      this.pLen = this.states.length;
     }
-    const sLen = this.states.length;
-    this.pLen = sLen;
-  }
 
-  Highlight() {
-    // // set x coordinate to be the x index of the tile * the size of the tile 
-    // let x = (this.pos.x * tileW);
-    // // set y coordinate to be the y index of the tile * the size of the tile 
-    // let y = (this.pos.y * tileH);
-    // // Set w, h to size
-    // let w = tileW;
-    // let h = tileH;
-    // noFill();
-    // stroke(124, 74, 232, 30);
-    // strokeWeight(2);
-    // rect(
-    //   x + tileSpacing / 2 + 1,
-    //   y + tileSpacing / 2 + 1,
-    //   w - tileSpacing - 2,
-    //   h - tileSpacing - 2,
-    //   tileBorderRadius
-    // );
   }
 }
